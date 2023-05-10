fbb2a7fed39ba9c7ac1475993f022623da4a30dc
shiitakedaisuki <129662461+shiitakedaisuki@users.noreply.github.com>
2023-05-10 10:04:30 +0000

    Add7

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,18 +1,18 @@
 class FavoritesController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     # 下のbook_idはここのbook_idのこと。下のbook.idのbookはここのbook=のbookのこと
-    favorite = current_user.favorites.new(book_id: book.id)
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path) 非同期通信するために削除する
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     #find_byは押すと消えたりついたりするシステムのこと。find_by(今から消そうとしている本の指定)
     favorite.destroy
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)　非同期通信するために削除する
   end
   
 end
